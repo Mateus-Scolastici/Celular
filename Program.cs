9b411622d459e7b83bab9f327e1b50cbf8c67ca2
Mateus-Scolastici <mateusfontascp2704@gmail.com>
2023-11-18 23:42:34 -0300

    Resolvendo desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia n1 = new Nokia("123", "Modelo 1", "00000000", 128);
+Iphone i1 = new Iphone("456", "Modelo 2", "11111111", 256);
+
+n1.Ligar();
+n1.InstalarAplicativo("WhatsApp");
+n1.ReceberLigacao();
+
+i1.Ligar();
+i1.InstalarAplicativo("Facebook");
+i1.ReceberLigacao();
